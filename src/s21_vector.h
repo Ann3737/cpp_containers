422b75ab3e107640685b75e1531af6440d509648
bortnikovaanna <bortnikovaanna@icloud.com>
2025-05-14 17:37:09 +0300

    realization some methods

--- src/s21_vector.h
+++ src/s21_vector.h
@@ -5,6 +5,7 @@
 #include <initializer_list>
 #include <stdexcept>
 #include <cstddef>
+#include <limits>
 #include <new>
 
 namespace s21{
@@ -33,10 +34,8 @@ public:
                 new (this->data_ + i) value_type();
             }
         } catch (...) {
-            for (size_type new_i = 0; new_i < i; ++i) {
-                this->data_[new_i].~value_type();
-            }
-            ::operator delete(this->data_);
+            this->size_ = i;
+            _DeallocateData();
             throw;
         }
     }
@@ -51,10 +50,8 @@ public:
                 ++i;
             }
         } catch (...) {
-            for (size_type new_i = 0; new_i < i; ++i) {
-                this->data_[new_i].~value_type();
-            }
-            ::operator delete(this->data_);
+            this->size_ = i;
+            _DeallocateData();
             throw;
         }
     }
@@ -68,12 +65,9 @@ public:
                 new (this->data_ + i) value_type(v.data_[i]);
             }
         } catch (...) {
-            for (size_type new_i = 0; new_i < i; ++i) {
-                this->data_[new_i].~value_type();
-            }
-            ::operator delete(this->data_);
+            this->size_ = i;
+            _DeallocateData();
             throw;
-
         }
     }
 
@@ -87,51 +81,76 @@ public:
 
     // Destructor
     ~vector() noexcept {
-        if (this->data_ != nullptr) {
-            for (size_type i = 0; i < this->size_; ++i) {
-                this->data_[i].~value_type();
-            }
-            ::operator delete(this->data_);
-        }
+        _DeallocateData();
     }
 
     // Element access
-    reference at(size_type pos);
-    vector& operator=(vector &&v) noexcept {
-        if (this != &v) {
-
-        }
-    }
-
-    // vector& operator=(const vector &v) noexcept {
-    //     if (this != &v) {
-    //         vector temp(v);
-    //         swap(temp);
-    //     }
-    //     return *this;
-    // }
-
-    reference operator[](size_type pos) {
+    reference at(size_type pos) {
         if (pos >= this->size_) {
             throw std::out_of_range("Index out of range");
         }
         return this->data_[pos];
     }
-    // const_reference operator[](size_type pos) const;
-    const_reference front() const;
-    const_reference back() const;
+
+    const_reference at(size_type pos) const {
+        if (pos >= this->size_) {
+            throw std::out_of_range("Index out of range");
+        }
+        return this->data_[pos];
+    }
+
+    vector& operator=(vector &&v) noexcept {
+        if (this != &v) {
+            _DeallocateData();
+            this->size_ = v.size_;
+            this->capacity_ = v.capacity_;
+            this->data_ = v.data_;
+
+            v.size_ = 0;
+            v.capacity_ = 0;
+            v.data_ = nullptr;
+        }
+        return *this;
+    }
+
+    vector& operator=(const vector &v) {        
+        if (this != &v) {
+            vector temp(v);
+            this->swap(temp);
+        }
+        return *this;
+    }
+
+    reference operator[](size_type pos) { return this->data_[pos]; }
+    const_reference operator[](size_type pos) const { return this->data_[pos]; }
+    const_reference front() const { return this->data_[0]; }
+    const_reference back() const { return this->data_[this->size_ - 1]; }
+    iterator data() { return this->data_; }
 
     // Iterators
-    iterator data();
-    iterator begin();
-    // const_iterator cbegin() const;
-    iterator end();
-    // const_iterator cend() const;
+    iterator begin() {
+        if (this->empty()) {
+            return this->end();
+        }
+        return this->data_;
+    }
+
+    const_iterator cbegin() const noexcept {
+        if (this->empty()) {
+            return this->end();
+        }
+        return this->data_;
+    }
+
+    iterator end() { return this->data_ + this->size_; }
+    const_iterator cend() const noexcept { return this->data_ + this->size_; }
 
     // Capacity
     bool empty() { return (this->size_ == 0); }
     size_type size() const { return this->size_; }
-    size_type max_size() const;
+    size_type max_size() const {
+        return std::numeric_limits<size_type>::max() / sizeof(value_type);
+    }
 
     // Reserve capacity memory
     void reserve(size_type size) {
@@ -152,11 +171,7 @@ public:
             throw;
         };
         
-        for (size_type i = 0; i < this->size_; ++i) {
-            this->data_[i].~value_type();
-        }
-
-        ::operator delete(this->data_);
+        _DeallocateData();
         this->data_ = newData;
         this->capacity_ = size;
     }
@@ -164,25 +179,27 @@ public:
     size_type capacity() const { return this->capacity_; }
     // void shrink_to_fit();
 
-    // // Modifiers
-    // void clear();
+    // Modifiers
+    void clear() {
+        for (size_type i = 0; i < this->size_; ++i) {
+            this->data_[i].~value_type();
+        }
+        this->size_ = 0;
+    }
     // iterator insert(iterator pos, const_reference value);
     // void erase(iterator pos);
+
     void push_back(const_reference value) {
         if (this->size_ == this->capacity_) {
             reserve(this->capacity_ > 0 ? this->capacity_ * 2 : 1);
         }
-        try {
-            new (this->data_ + this->size_) value_type(value);
-        } catch (...) {
-            throw;
-        }
-
+        new (this->data_ + this->size_) value_type(value);
         ++this->size_;
     }
     // void pop_back();
+
     void swap(vector& other) {
-        if (this != other) {
+        if (this != &other) {
             std::swap(this->size_, other.size_);
             std::swap(this->capacity_, other.capacity_);
             std::swap(this->data_, other.data_);
@@ -196,6 +213,15 @@ private:
 
     iterator _AllocateData(size_type cap) {
         return reinterpret_cast<iterator>(::operator new(sizeof(value_type) * cap));
+    }
+
+    void _DeallocateData() {
+        if (this->data_ != nullptr) {
+            for (size_type i = 0; i < this->size_; ++i) {
+                this->data_[i].~value_type();
+            }
+            ::operator delete(this->data_);
+        }
     }
 };
 }
